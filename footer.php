502d273f9a3ab4dfc65389739dd0b3c1e9fba66b
Shahed Amer <chanyeol6521354@gmail.com>
2023-05-08 09:32:58 +0200

    BO

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer>
+<!-- <footer>
     <div class="left-footer">
         <p>Home</p>
         <p>About Us</p>
@@ -14,6 +14,29 @@
     </div>
     <div class="line-right"></div>
     <div class="right-footer">
+        <p>06-1234567
+            <br> skateroom@gmail.com
+        </p>
+        <p>3526 AB Utrecht
+            <br> Australielaan 25
+        </p>
+    </div>
+</footer> -->
+
+<footer>
+    <div class="sitemap">
+        <p>Home</p>
+        <p>About Us</p>
+        <p>Products</p>
+    </div>
+    <div class="social">
+        <img src="/IMG/logos/youtube (2).png" alt="youtube-icon">
+        <img src="/IMG/logos/facebook.png" alt="facebook-icon">
+        <img src="/IMG/logos/instagram.png" alt="instagram-icon">
+        <img src="/IMG/logos/tik-tok.png" alt="tik-tok_icon">
+        <img src="/IMG/logos/twitter (2).png" alt="twitter-icon">
+    </div>
+    <div class="address">
         <p>06-1234567
             <br> skateroom@gmail.com
         </p>
