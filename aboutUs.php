7282a25be0d499bfacda9b15ea0d1a4f7f26a630
Shahed Amer <chanyeol6521354@gmail.com>
2023-05-08 11:50:26 +0200

    aboutUs HTML is af

--- aboutUs.php
+++ aboutUs.php
@@ -1,2 +1,25 @@
 <?php include "header.php"; ?>
-  <?php include "footer.php"; ?>
+<section class="aboutUs">
+  <div class="aboutUs-banner">
+    <img src="/IMG/personeel.jpg" alt="personeel-png">
+    <h1>We value art. We value skate. We value social change</h1>
+  </div>
+  <div class="BCorp">
+    <p>Sinds 2014 werkt THE SKATEROOM nauw samen met 's werelds meest invloedrijke artiesten om internationale skate- en onderwijs non-profitorganisaties en -projecten te ondersteunen.
+
+      Als trotse B Corp omarmen we een sociaal ondernemerschapsmodel, terwijl we actief werken aan het verbeteren van duurzaamheid en transparantie in ons werk.
+
+    </p>
+    <img src="/IMG/Bcorporation.png" alt="">
+
+  </div>
+  <div class="ontwikkelingen-text">
+    <img src="/IMG/global.png" alt="globalCompact-png">
+    <img src="/IMG/goals.png" alt="goals-png">
+    <p>We verbinden ons aan de organisaties die we financieren: minstens 10% van alle inkomsten bij THE SKATEROOM gaat rechtstreeks naar het creëren van sociale verandering. We helpen jongeren over de hele wereld te empoweren, nieuwe skateparken te bouwen en onderwijsfaciliteiten en hulpmiddelenprogramma's te ontwikkelen.
+
+      Door limited editions van THE SKATEROOM te kopen, hebben onze verzamelaars en gemeenschap een directe rol in het creëren van blijvende sociale verandering. Samen werken we aan een betere toekomst voor het consumentisme.</p>
+  </div>
+</section>
+
+<?php include "footer.php"; ?>
