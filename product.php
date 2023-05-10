8b808f02422a8dccdc36a47c1f3d830168753f2f
Shahed Amer <chanyeol6521354@gmail.com>
2023-05-10 11:26:57 +0200

    product.php is af

--- product.php
+++ product.php
@@ -1,2 +1,54 @@
 <?php include "header.php"; ?>
-  <?php include "footer.php"; ?>
+<div class="all-products">
+  <div class="product-row">
+    <div><img src="/IMG/skateboarden/haring-manandmedusa-1300px.jpg" alt="">
+      <h5>MAN AND MEDUSA,</h5>
+      <p>KEITH HARING <br>
+        €700</p>
+    </div>
+  </div>
+  <div class="product-row">
+    <div><img src="/IMG/skateboarden/haring-monsters-1300px.jpg" alt="monster-skateboard">
+      <h5>MONSTERS, </h5>
+      <p>KEITH HARING <br>
+        €810</p>
+      <div>
+        <img src="/IMG/skateboarden/warhol-sunset-orange-1300px.jpg" alt="sunsetOrange-skateboard">
+        <h5>SUNSET – ORANGE,</h5>
+        <p>ANDY WARHOL <br>
+          €600</p>
+      </div>
+    </div>
+    <div><img src="/IMG/skateboarden/warhol-sunset-purple-1300px-1000x1131.jpg" alt="sunsetPurple-skateboard">
+      <h5>SUNSET – PURPLE, </h5>
+      <p>ANDY WARHOL <br>
+        €600</p>
+    </div>
+  </div>
+
+  <div class="product-row">
+    <div><img src="/IMG/skateboarden/koons-popeye-1300px.jpg" alt="popeye-skateboard">
+      <h5>POPEYE SKATEBOARD, </h5>
+      <p>JEFF KOONS <br>
+        €950</p>
+    </div>
+  </div>
+  <div class="product-row">
+    <div><img src="/IMG/skateboarden/koons-balloonvenus-1300px.jpg" alt="balloon-skateboard">
+      <h5>BALLOON VENUSSKATEBOARD,</h5>
+      <p> JEFF KOONS <br>
+        €700</p>
+    </div>
+    <div><img src="/IMG/skateboarden/waynethiebaud_wb_edit_26_1300px-2-1-1000x1000.jpg" alt="cones-skateboard">
+      <h5>THREE CONES,</h5>
+      <p>WAYNE THIEBAUD <br>
+        €600</p>
+    </div>
+    <div><img src="/IMG/skateboarden/warhol-coloredcampbells-boxset-1300px.jpg" alt="soup-skateboard">
+      <h5>SET OF 8 COLORED CAMPBELL’S SOUP CANS,</h5>
+      <p>ANDY WARHOL <br>
+        €1650</p>
+    </div>
+  </div>
+</div>
+<?php include "footer.php"; ?>
