b3852ebd1f9ae3aa14d4a06f036cd1dc448e0061
Shahed Amer <chanyeol6521354@gmail.com>
2023-05-12 23:35:26 +0200

    ProductenPagina.php is af en aan de styling begonnen

---
+++ productenPagina.php
@@ -0,0 +1,26 @@
+<?php include "header.php"; ?>
+<div class="productenPage">
+    <img src="/IMG/skateboarden/warhol-sunset-purple-1300px-1000x1131.jpg" alt="purple-skateboard">
+    <h4>SUNSET – PURPLE,</h4>
+    <p>ANDY WARHOL <br>
+        <strong> €600</strong>
+    </p>
+    <input type="button" value="ADD TO CART">
+    <p>
+        <strong>Beschrijving: </strong>
+        In het begin van de jaren zestig creëerde Andy Warhol veel werken met consumentenproducten, Hollywoodsterren en een veelbesproken serie die dood en rampspoed in de Amerikaanse samenleving illustreert.
+
+        Als een van de meest erkende gezichten in de hedendaagse kunst, leek het voor ons vanzelfsprekend om te proberen de visie van Andy Warhol te bestendigen door middel van onze productie. Daarom hebben we onlangs samengewerkt met de Andy Warhol Foundation for the Visual Arts om een reeks van Andy's meest iconische werken te produceren, waaronder het Sunset oranje drieluik op skateboards.
+        Onder licentie van The Andy Warhol Foundation for the Visual Arts, Inc.
+    </p>
+    <p><strong>over product:</strong>
+        Oplage van 500
+        Geproduceerd en genummerd door The Skateroom in 2019
+        Onder licentie van The Andy Warhol Foundation for the Visual Arts, Inc.
+        Sunset Triptych set van 3 skateboards
+        Elk dek meet ca. 31x8in (80x20cm)
+        Gemaakt van 7-laags Grade A Canadees esdoornhout
+        Top-print bevat de officiële Andy Warhol brand logo
+        1 Easyfix wall mount inbegrepen per deck</p>
+</div>
+<?php include "footer.php"; ?>
