29d3031d6a9b418c569c986056555fa6ea4646a2
Shahed Amer <chanyeol6521354@gmail.com>
2023-05-12 23:33:07 +0200

    Product page en styling is af

--- product.php
+++ product.php
@@ -1,54 +1,88 @@
 <?php include "header.php"; ?>
 <div class="all-products">
   <div class="product-row">
-    <div><img src="/IMG/skateboarden/haring-manandmedusa-1300px.jpg" alt="">
-      <h5>MAN AND MEDUSA,</h5>
-      <p>KEITH HARING <br>
-        €700</p>
+    <div class="KEITH-HARING-collection">
+      <div class="product">
+        <div class="shadow">
+          <a href="#">
+            <img src="/IMG/skateboarden/haring-manandmedusa-1300px.jpg" alt="">
+          </a>
+        </div>
+
+        <h5>MAN AND MEDUSA,</h5>
+        <p>KEITH HARING <br>
+          €700</p>
+      </div>
+      <div class="product">
+        <div class="shadow">
+          <img src="/IMG/skateboarden/haring-monsters-1300px.jpg" alt="monster-skateboard">
+        </div>
+        <h5>MONSTERS, </h5>
+        <p>KEITH HARING <br>
+          €810</p>
+      </div>
     </div>
-  </div>
-  <div class="product-row">
-    <div><img src="/IMG/skateboarden/haring-monsters-1300px.jpg" alt="monster-skateboard">
-      <h5>MONSTERS, </h5>
-      <p>KEITH HARING <br>
-        €810</p>
-      <div>
-        <img src="/IMG/skateboarden/warhol-sunset-orange-1300px.jpg" alt="sunsetOrange-skateboard">
+    <div class="ANDY-WARHOL-collection">
+      <div class="product">
+        <div class="shadow">
+          <img src="/IMG/skateboarden/warhol-sunset-orange-1300px.jpg" alt="sunsetOrange-skateboard">
+        </div>
         <h5>SUNSET – ORANGE,</h5>
         <p>ANDY WARHOL <br>
           €600</p>
       </div>
+
+      <div class="product">
+        <div class="shadow">
+          <a href="/productenPagina.php">
+            <img src="/IMG/skateboarden/warhol-sunset-purple-1300px-1000x1131.jpg" alt="sunsetPurple-skateboard">
+          </a>
+        </div>
+        <h5>SUNSET – PURPLE, </h5>
+        <p>ANDY WARHOL <br>
+          €600</p>
+      </div>
     </div>
-    <div><img src="/IMG/skateboarden/warhol-sunset-purple-1300px-1000x1131.jpg" alt="sunsetPurple-skateboard">
-      <h5>SUNSET – PURPLE, </h5>
-      <p>ANDY WARHOL <br>
-        €600</p>
+  </div>
+  <div class="product-row">
+    <div class="JEFF-KOONS-collection">
+      <div class="product">
+        <div class="shadow">
+          <img src="/IMG/skateboarden/koons-popeye-1300px.jpg" alt="popeye-skateboard">
+        </div>
+        <h5>POPEYE SKATEBOARD, </h5>
+        <p>JEFF KOONS <br>
+          €950</p>
+      </div>
+      <div class="product">
+        <div class="shadow">
+          <img class="balloon" src="/IMG/skateboarden/koons-balloonvenus-1300px.jpg" alt="balloon-skateboard">
+        </div>
+        <h5>BALLOON VENUSSKATEBOARD,</h5>
+        <p> JEFF KOONS <br>
+          €700</p>
+      </div>
+    </div>
+    <div class="WAYNE-THIEBAUD-collection">
+      <div class="product">
+        <div class="shadow">
+          <img src="/IMG/skateboarden/waynethiebaud_wb_edit_26_1300px-2-1-1000x1000.jpg" alt="cones-skateboard">
+        </div>
+        <h5>THREE CONES,</h5>
+        <p>WAYNE THIEBAUD <br>
+          €600</p>
+      </div>
+      <div class="product">
+        <div class="shadow">
+          <img src="/IMG/skateboarden/warhol-coloredcampbells-boxset-1300px.jpg" alt="soup-skateboard">
+        </div>
+        <h5>SET OF 8 COLORED CAMPBELL’S SOUP CANS,</h5>
+        <p>ANDY WARHOL <br>
+          €1650</p>
+      </div>
     </div>
   </div>
 
-  <div class="product-row">
-    <div><img src="/IMG/skateboarden/koons-popeye-1300px.jpg" alt="popeye-skateboard">
-      <h5>POPEYE SKATEBOARD, </h5>
-      <p>JEFF KOONS <br>
-        €950</p>
-    </div>
-  </div>
-  <div class="product-row">
-    <div><img src="/IMG/skateboarden/koons-balloonvenus-1300px.jpg" alt="balloon-skateboard">
-      <h5>BALLOON VENUSSKATEBOARD,</h5>
-      <p> JEFF KOONS <br>
-        €700</p>
-    </div>
-    <div><img src="/IMG/skateboarden/waynethiebaud_wb_edit_26_1300px-2-1-1000x1000.jpg" alt="cones-skateboard">
-      <h5>THREE CONES,</h5>
-      <p>WAYNE THIEBAUD <br>
-        €600</p>
-    </div>
-    <div><img src="/IMG/skateboarden/warhol-coloredcampbells-boxset-1300px.jpg" alt="soup-skateboard">
-      <h5>SET OF 8 COLORED CAMPBELL’S SOUP CANS,</h5>
-      <p>ANDY WARHOL <br>
-        €1650</p>
-    </div>
-  </div>
 </div>
+
 <?php include "footer.php"; ?>
